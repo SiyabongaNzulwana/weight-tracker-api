d1581389ece9178ebf6ce512edce2ed74f7e4990
Siyabonga Nzulwana <siyabonga@linumlabs.com>
2023-03-01 01:16:02 +0200

    fixed empty req.body issue

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const config = require('config')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 const express = require('express')
 const app = express()
 const logger = require('morgan')
 const mongoose = require('mongoose')
 const userRouter = require('./src/routes/user.route')
 const weight = require('./src/routes/weight.route')
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 if (!config.get('jwtPrivateKey')) {
   console.error('FATAL ERROR: jwtPrivateKey is not defined')
@@ -17,13 +23,9 @@ mongoose
   .then(
     () => {
       console.log('Successfully connected to DB')
-      const port = process.env.PORT || 3000
+      const port = process.env.PORT || 3001
       const userUrl = '/api/users'
       app.use(logger('dev')) // log requests to the console
-
-      // Parse incoming requests data
-      app.use(express.json())
-
       // load our API routes
       app.use(userUrl, userRouter)
       app.use(userUrl, weight)
