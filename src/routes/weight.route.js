98a0a35dfcebd8773c0668ddf9a578b11431a1d9
Siyabonga Nzulwana <siyabonga@linumlabs.com>
2023-03-02 02:04:05 +0200

    protected some routes

--- src/routes/weight.route.js
+++ src/routes/weight.route.js
@@ -2,9 +2,19 @@ const express = require('express')
 const weightController = require('../controllers/weight.controller')
 const router = express.Router()
 
-router.post('/save_weight', weightController.addWeight)
-router.get('/get_weight_history', weightController.getUserWeightRecords)
-router.delete('/delete_weight/:id', weightController.removeUserWeightRecord)
-router.put('/update_weight/:id', weightController.updateUserWeightRecord)
+const auth = require('../middleware/auth')
+
+router.post('/save_weight', auth, weightController.addWeight)
+router.get(
+  '/get_weight_history/:email',
+  auth,
+  weightController.getUserWeightRecords
+)
+router.delete(
+  '/delete_weight/:id',
+  auth,
+  weightController.removeUserWeightRecord
+)
+router.put('/update_weight/:id', auth, weightController.updateUserWeightRecord)
 
 module.exports = router
