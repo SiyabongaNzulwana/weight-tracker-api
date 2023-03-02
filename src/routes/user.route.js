1597a649ecfb00b61c6a4759ee7f054f9b11e504
Siyabonga Nzulwana <siyabonga@linumlabs.com>
2023-03-02 02:04:37 +0200

    removed auth on login

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -5,7 +5,7 @@ const { User } = require('../models/user.model')
 const router = express.Router()
 
 router.post('/sign_up', userController.registerUser)
-router.post('/login', auth, userController.signIn)
+router.post('/login', userController.signIn)
 
 router.get('/me', auth, async (req, res) => {
   const user = await User.findById(req.user._id).select('-password')
